99173a9bdd086410cd954855630fec3ec06e96ff
Gaming32 <31409929+Gaming32@users.noreply.github.com>
2019-02-21 19:48:05 -0600

    2/21/2019

--- ZipFile_FindReplace.py
+++ ZipFile_FindReplace.py
@@ -1,2 +1,18 @@
+import sys
 from zipfile import *
-z = ZipFile('ORBIT3R-BOT.ev3', 'wr')
+z = ZipFile(sys.argv[1])
+zw = []
+end = sys.argv[4] if len(sys.argv)>4 else ''
+for f in (f for f in z.namelist() if f.endswith(end)):
+	print(f)
+	#.lower().find(sys.argv[2].encode())
+	#for match in z.open(f).read().lower().find(sys.argv[2].encode()):
+	#	print(bytes([match]).decode(), end='')
+	b = z.read(f).lower().decode()
+	#print(b)
+	b = b.replace(sys.argv[2], sys.argv[3])
+	zw.append((f, b))
+z.close()
+z = ZipFile(sys.argv[1], 'w')
+for (f, b) in zw:
+	z.writestr(f, b)
